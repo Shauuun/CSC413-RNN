55e9522014a5d83be95d1c11685700903240caf4
Zechuan Liu <percyliuzechuan@hotmail.com>
2023-04-03 18:01:16 -0400

    update RNN

--- RNN_Network.py
+++ RNN_Network.py
@@ -6,28 +6,38 @@ import math
 
 
 class SelfAttention(nn.Module):
-    def __init__(self, hidden_dim):
+    def __init__(self, d_model, num_heads):
         super(SelfAttention, self).__init__()
-        self.Wk = nn.Linear(hidden_dim, hidden_dim)
-        self.Wq = nn.Linear(hidden_dim, hidden_dim)
-        self.V = nn.Linear(hidden_dim, hidden_dim)
+        self.Wk = nn.Linear(d_model, d_model)
+        self.Wq = nn.Linear(d_model, d_model)
+        self.V = nn.Linear(d_model, d_model)
+        self.Wh = nn.Linear(d_model, d_model)
+        self.head = num_heads
+        self.d_k = d_model // self.head
 
     def forward(self, x):
+        batch_size = x.shape[0]
         query = self.Wq(x)
         key = self.Wk(x)
         value = self.V(x)
 
-        d_k = query.size(-1)
+        Q = query.view(batch_size, -1, self.head, self.d_k).transpose(1, 2)
+        K = key.view(batch_size, -1, self.head, self.d_k).transpose(1, 2)
+        V = value.view(batch_size, -1, self.head, self.d_k).transpose(1, 2)
 
-        attention_logits = torch.matmul(query, key.transpose(-2, -1))
-        attention_weights = F.softmax(attention_logits / math.sqrt(d_k), dim=-1)
-        attention_output = torch.matmul(attention_weights, value)
+        attention_logits = torch.matmul(Q, K.transpose(-2, -1))
+        attention_weights = F.softmax(attention_logits / math.sqrt(self.d_k), dim=-1)
+        attention_output = torch.matmul(attention_weights, V)
+
+        attention_output = attention_output.transpose(1, 2).contiguous().view(batch_size, -1, self.head * self.d_k)
+        attention_output = self.Wh(attention_output)
 
         return attention_output, attention_weights
 
 
 class RNNAttention(nn.Module):
-    def __init__(self, vocab_size, embedding_dim, hidden_dim, output_dim, n_layers=1, bidirectional=True, dropout=0):
+    def __init__(self, vocab_size, embedding_dim, hidden_dim, output_dim, num_heads, n_layers=1,
+                 bidirectional=True, dropout=0):
         super().__init__()
         self.hidden_dim = hidden_dim
         self.embedding = nn.Embedding(vocab_size, embedding_dim)
@@ -35,7 +45,7 @@ class RNNAttention(nn.Module):
                            batch_first=True)
         self.fc1 = nn.Linear(hidden_dim * 2, hidden_dim)
         self.fc2 = nn.Linear(hidden_dim, output_dim)
-        self.attention = SelfAttention(embedding_dim)
+        self.attention = SelfAttention(embedding_dim, num_heads)
 
     def forward(self, text):
         embedded = self.embedding(text)
